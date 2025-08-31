6c82366f8c2067fcc625dcd325ed97d13a7d7587
Youssef Itani <yitani@c1r7s4.42beirut.com>
2025-08-31 20:11:09 +0300

    g

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/31 00:00:00 by yitani            #+#    #+#             */
+/*   Updated: 2025/08/31 16:17:11 by yitani           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat
+{
+	private:
+		const std::string	name;
+		int					grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
+		~Bureaucrat();
+
+		const std::string&	getName() const;
+		int					getGrade() const;
+
+		void				incrementGrade();
+		void				decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif

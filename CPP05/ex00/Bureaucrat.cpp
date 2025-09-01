17e4246ce5cae10558000c2d613ba84d55c734e2
Youssef <youssef.itani.11.11@gmail.com>
2025-09-01 22:44:14 +0300

    small commit

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,12 +6,30 @@
 /*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 15:45:17 by yitani            #+#    #+#             */
-/*   Updated: 2025/08/31 19:30:51 by yitani           ###   ########.fr       */
+/*   Updated: 2025/09/01 22:43:30 by yitani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <Bureaucrat.hpp>
 
 Bureaucrat::Bureaucrat(void)	:	name("default"), grade(75)
+{
+	std::cout << "Bureacrat default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)	:	name(name), grade(grade)
+{
+	std::cout << "Bureacrat :" << name << " constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+{
+}
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat& other)
+{
+}
+
+Bureaucrat::~Bureaucrat(void)
 {
 }

baf04febe88896ba0d43d40d9c4571e4bb9ff3eb
Youssef <youssef.itani.11.11@gmail.com>
2025-09-07 02:10:00 +0300

    main is missing

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -6,8 +6,82 @@
 /*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 17:58:46 by yitani            #+#    #+#             */
-/*   Updated: 2025/09/04 23:47:54 by yitani           ###   ########.fr       */
+/*   Updated: 2025/09/07 02:08:30 by yitani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
+
+AForm *Intern::createShrubberyCreationForm(const std::string &target) const
+{
+	return (new ShrubberyCreationForm(target));
+}
+
+AForm *Intern::createRobotomyRequestForm(const std::string &target) const
+{
+	return (new RobotomyRequestForm(target));
+}
+
+AForm *Intern::createPresidentialPardonForm(const std::string &target) const
+{
+	return (new PresidentialPardonForm(target));
+}
+
+Intern::Intern(void) : formCreators{&Intern::createShrubberyCreationForm,
+									&Intern::createRobotomyRequestForm,
+									&Intern::createPresidentialPardonForm},
+					   formNames{"shrubbery creation", "robotomy request", "presidential pardon"}
+{
+
+	std::cout << "Intern default constructor called" << std::endl;
+}
+
+Intern::Intern(const Intern &other)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		formCreators[i] = other.formCreators[i];
+		formNames[i] = other.formNames[i];
+	}
+
+	std::cout << "Intern copy constructor called" << std::endl;
+}
+
+Intern &Intern::operator=(const Intern &other)
+{
+	if (this != &other)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			formCreators[i] = other.formCreators[i];
+			formNames[i] = other.formNames[i];
+		}
+	}
+
+	std::cout << "Intern copy assignment operator called" << std::endl;
+
+	return (*this);
+}
+
+Intern::~Intern(void)
+{
+	std::cout << "Intern destructor called" << std::endl;
+}
+
+AForm*	Intern::makeForm(const std::string &formName, const std::string &target)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		if (formName == formNames[i])
+		{
+			std::cout << "Intern creates " << formName << std::endl;
+			return ((this->*formCreators[i])(target));
+		}
+	}
+	throw (Intern::FormNotFoundException());
+}
+
+const char *Intern::FormNotFoundException::what() const throw()
+{
+	return ("Form not found");
+}
--- CPP05/ex03/Intern.hpp
+++ CPP05/ex03/Intern.hpp
@@ -5,14 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/04 17:58:52 by yitani            #+#    #+#             */
-/*   Updated: 2025/09/04 23:48:21 by yitani           ###   ########.fr       */
+/*   Created: 2025/09/05 00:00:00 by yitani            #+#    #+#             */
+/*   Updated: 2025/09/07 01:12:12 by yitani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERN_HPP
 # define INTERN_HPP
 
+# include <iostream>
+# include <string>
+# include "AForm.hpp"
+# include "ShrubberyCreationForm.hpp"
+# include "RobotomyRequestForm.hpp"
+# include "PresidentialPardonForm.hpp"
 
+class Intern
+{
+	private:
+		AForm* (Intern::*formCreators[3])(const std::string& target) const;
+        
+        std::string formNames[3];
+
+		AForm*	createShrubberyCreationForm(const std::string& target) const;
+		AForm*	createRobotomyRequestForm(const std::string& target) const;
+		AForm*	createPresidentialPardonForm(const std::string& target) const;
+
+	public:
+		Intern();
+		Intern(const Intern& other);
+		Intern& operator=(const Intern& other);
+		~Intern();
+
+		AForm*	makeForm(const std::string& formName, const std::string& target);
+
+		class FormNotFoundException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
 
 #endif

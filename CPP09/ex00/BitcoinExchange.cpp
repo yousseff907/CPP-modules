5bcbb427ec521c17280fa177586524cd924b347b
Youssef <youssef.itani.11.11@gmail.com>
2025-12-22 20:06:31 +0200

    ex00

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,36 @@
 /*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:51:21 by yitani            #+#    #+#             */
-/*   Updated: 2025/10/15 12:51:22 by yitani           ###   ########.fr       */
+/*   Updated: 2025/12/22 19:47:50 by yitani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)	:	Content(other.Content)	{}
+
+BitcoinExchange&	BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+	if (this != &other)
+		Content = other.Content;
+	return (*this);
+}
+
+bool	BitcoinExchange::parseAndStore(std::string fileName)
+{
+	std::ifstream	data(fileName);
+	std::string	line;
+
+	if (!data.is_open())
+		return (false);
+
+	std::getline(data, line);
+	while (std::getline(data, line))
+	{
+
+	}
+}
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,30 @@
 /*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:51:19 by yitani            #+#    #+#             */
-/*   Updated: 2025/10/15 12:51:20 by yitani           ###   ########.fr       */
+/*   Updated: 2025/12/22 19:38:31 by yitani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <map>
+# include <iostream>
+# include <string>
+# include <fstream>
+
+class BitcoinExchange
+{
+	private:
+		std::map<std::string, double> Content;
+		
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &other);
+		BitcoinExchange&	operator=(const BitcoinExchange &other);
+		~BitcoinExchange();
+		bool	parseAndStore(std::string fileName);
+		
+};
+
+#endif
